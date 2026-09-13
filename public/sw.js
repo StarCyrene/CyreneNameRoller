7f9ec2265ce20c45a008ca02227afd0099acf0d5
Cyrene2008 <180080264+Cyrene2008@users.noreply.github.com>
2026-09-13 09:03:24 +0800

    chore: bump version to 26.3.0，补充更新日志与版本公告

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cyrene-v26.2.2-shell-2'
+const CACHE_NAME = 'cyrene-v26.3.0-shell-2'
 const ASSETS = [
   '/',
   '/index.html',
--- src/utils/version.js
+++ src/utils/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '26.2.2'
+export const APP_VERSION = '26.3.0'
 export const APP_VERSION_PREFIX = 'v'
 
 function getBuildNumber() {
